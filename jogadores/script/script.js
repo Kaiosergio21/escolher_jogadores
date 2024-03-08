ba916bda60fdcc280e59b395e490fd84b7fdf428
Kaio Sérgio Sales Nunes <137112682+Kaiosergio21@users.noreply.github.com>
2024-03-08 10:35:39 -0300

    Add files via upload

---
+++ jogadores/script/script.js
@@ -0,0 +1,51 @@
+const jogador = document.querySelector('#jogador');
+
+jogador.addEventListener('click', escolher);
+
+function messi() {
+       const jogador = document.querySelector('#jogador');
+       if(jogador.src.includes("image/background.jpg")) {
+           jogador.src = "image/messi.png";
+       } else {
+           jogador.src = "image/background.jpg";
+       }
+   }
+
+
+   function messi() {
+       const jogador = document.querySelector('#jogador');
+       if(jogador.src.includes("image/background.jpg")) {
+           jogador.src = "image/messi.png";
+       } else {
+           jogador.src = "image/background.jpg";
+       }
+   }
+
+
+   function neymar() {
+       const jogador = document.querySelector('#jogador');
+       if(jogador.src.includes("image/background.jpg")) {
+           jogador.src = "image/neymar.png";
+       } else {
+           jogador.src = "image/background.jpg";
+       }
+   }
+
+
+   function cristiano() {
+       const jogador = document.querySelector('#jogador');
+       if(jogador.src.includes("image/background.jpg")) {
+           jogador.src = "image/cristiano.png";
+       } else {
+           jogador.src = "image/background.jpg";
+       }
+   }
+
+   function mbappe() {
+       const jogador = document.querySelector('#jogador');
+       if(jogador.src.includes("image/background.jpg")) {
+           jogador.src = "image/Mbappe.png";
+       } else {
+           jogador.src = "image/background.jpg";
+       }
+   }
